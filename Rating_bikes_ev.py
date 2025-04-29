6e5bf6118b24f0daf58a89417f669eb60dbdf5e4
Harindranath1005 <harindranath96@gmail.com>
2025-04-29 08:24:10 +0530

    updated .getenv

--- Rating_bikes_ev.py
+++ Rating_bikes_ev.py
@@ -6,11 +6,11 @@ import pandas as pd
 #connection to database using .env variables
 load_dotenv()
 
-DB_HOST = st.secrets['DB_HOST']
-DB_PORT = st.secrets['DB_PORT']
-DB_NAME = st.secrets['DB_NAME']
-DB_USER = st.secrets['DB_USER']
-DB_PASSWORD = st.secrets['DB_PASSWORD']
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
 mydb = mysql.connector.connect(
     host=DB_HOST,
     port=DB_PORT,
